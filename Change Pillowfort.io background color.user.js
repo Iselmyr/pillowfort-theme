0995e485e62fa9309658afd6e88a72f2033a8f7b
Iselmyr <41116277+Iselmyr@users.noreply.github.com>
2018-07-11 07:48:46 -0500

    Add files via upload

---
+++ Change Pillowfort.io background color.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Change Pillowfort.io background color
+// @namespace   https://pillowfort.io
+// @description Changes the background color of Pillowfort.io pages
+// @include     https://pillowfort.io/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+document.body.style.background = "#451445";
